78bdced69bd4a2373ed4ee5cf618e02b054ddb24
Olga Kladova <Olenka-29-04@yandex.ru>
2025-06-17 19:29:43 +0300

    add filter

---
+++ src/common/all-exception.filter.ts
@@ -0,0 +1,65 @@
+import {
+  ExceptionFilter,
+  Catch,
+  ArgumentsHost,
+  HttpStatus,
+  PayloadTooLargeException,
+  HttpException,
+} from '@nestjs/common';
+import { Response } from 'express';
+import { EntityNotFoundError } from 'typeorm/error/EntityNotFoundError';
+
+@Catch()
+export class AllExceptionFilter implements ExceptionFilter {
+  catch(exception: unknown, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+
+    if (exception instanceof EntityNotFoundError) {
+      return response.status(HttpStatus.NOT_FOUND).json({
+        statusCode: HttpStatus.NOT_FOUND,
+        message: 'Cущность не найдена',
+      });
+    }
+
+    if (
+      typeof exception === 'object' &&
+      exception !== null &&
+      'code' in exception &&
+      exception.code === '23505'
+    ) {
+      return response.status(HttpStatus.CONFLICT).json({
+        statusCode: HttpStatus.CONFLICT,
+        message: 'Пользователь с таким email уже существует',
+      });
+    }
+
+    if (
+      exception instanceof PayloadTooLargeException ||
+      (typeof exception === 'object' &&
+        exception !== null &&
+        'name' in exception &&
+        exception.name === 'PayloadTooLargeException')
+    ) {
+      return response.status(HttpStatus.PAYLOAD_TOO_LARGE).json({
+        statusCode: HttpStatus.PAYLOAD_TOO_LARGE,
+        message: 'Файл слишком большой',
+      });
+    }
+
+    const status =
+      exception instanceof HttpException
+        ? exception.getStatus()
+        : HttpStatus.INTERNAL_SERVER_ERROR;
+
+    const message =
+      exception instanceof HttpException
+        ? exception.getResponse()
+        : 'Internal server error';
+
+    return response.status(status).json({
+      statusCode: status,
+      message,
+    });
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,13 @@ import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 import { ConfigService } from '@nestjs/config';
 import { logger } from './config/mainLogger';
+import { AllExceptionFilter } from './common/all-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   app.use(cookieParser());
+  app.useGlobalFilters(new AllExceptionFilter());
   const port = configService.get<number>('port') as number;
   await app.listen(port);
   logger.log(`app listen port: ${port}`);
