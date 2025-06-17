6ad1aaa9dbb5bc35343cc9420c20c9d3c50ae7a9
romanbakurov <ibakur@ya.ru>
2025-06-17 12:33:42 +0300

    Добавил пайпу валидации ValidationPipe в main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,22 @@ import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 import { ConfigService } from '@nestjs/config';
 import { logger } from './config/mainLogger';
+import {ValidationPipe} from "@nestjs/common";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
+
   app.use(cookieParser());
+
+  app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: true,
+        transform: true,
+      })
+  )
+
   const port = configService.get<number>('port') as number;
   await app.listen(port);
   logger.log(`app listen port: ${port}`);
