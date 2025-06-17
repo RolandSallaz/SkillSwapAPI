8579eba0013f74322e69c4b8a7759c474c35d522
romanbakurov <ibakur@ya.ru>
2025-06-17 13:30:27 +0300

    fix:  закомментированы createSkillDto и UpdateSkillDto в skills.controller.ts

--- src/skills/skills.controller.ts
+++ src/skills/skills.controller.ts
@@ -1,16 +1,24 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  // Body,
+  // Patch,
+  Param,
+  Delete
+} from '@nestjs/common';
 import { SkillsService } from './skills.service';
-import { CreateSkillDto } from './dto/create-skill.dto';
-import { UpdateSkillDto } from './dto/update-skill.dto';
+// import { CreateSkillDto } from './dto/create-skill.dto';
+// import { UpdateSkillDto } from './dto/update-skill.dto';
 
 @Controller('skills')
 export class SkillsController {
   constructor(private readonly skillsService: SkillsService) {}
 
   @Post()
-  create(@Body() createSkillDto: CreateSkillDto) {
-    return this.skillsService.create(createSkillDto);
-  }
+  // create(@Body() createSkillDto: CreateSkillDto) {
+  //   return this.skillsService.create(createSkillDto);
+  // }
 
   @Get()
   findAll() {
@@ -22,10 +30,10 @@ export class SkillsController {
     return this.skillsService.findOne(+id);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateSkillDto: UpdateSkillDto) {
-    return this.skillsService.update(+id, updateSkillDto);
-  }
+  // @Patch(':id')
+  // update(@Param('id') id: string, @Body() updateSkillDto: UpdateSkillDto) {
+  //   return this.skillsService.update(+id, updateSkillDto);
+  // }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
