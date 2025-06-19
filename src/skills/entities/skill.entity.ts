3db5558253330aa7f5fe233827f30b21011da573
Roman Bakurov <ibakur@yandex.ru>
2025-06-19 21:07:53 +0300

    Week2 roman.bakurov (skill enitity)
    * добавил в config/configuration.ts параметр NODE_ENV, который должен быть по умолчанию значением 'development', добавил данный параметр в .env.example файл
    
    * В ormconfig сделал параметр syncronize - true только в dev режиме, реализовал проверку на NODE_ENV !== 'production'
    
    * вынесены интерфейсы из accessToken.guard.ts в отдельный файл в auth/types.ts
    
    * вынесены enum в отдельный файл в users/enums.ts из create.users.dto.ts и в users.entity.ts
    
    * параметр NODE_ENV добавлен в .env.example файл
    
    * Добавил пайпу валидации ValidationPipe в main.ts
    
    * feat: описано entity для навыков согласно тз, в enitity users добавлено поле skills и связаны сущности
    
    * fix: исправлены импорты AuthRequest в файлах auth.controller.ts и users.controller.ts
    
    * fix: исправлено после ревью skill.entity.ts и запущен линтер с фиксом
    
    ---------
    
    Co-authored-by: romanbakurov <ibakur@ya.ru>
    Co-authored-by: Roland <machodin@yandex.ru>

--- src/skills/entities/skill.entity.ts
+++ src/skills/entities/skill.entity.ts
@@ -1,1 +1,27 @@
-export class Skill {}
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { User } from '../../users/entities/users.entity';
+
+@Entity()
+export class Skill {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  title: string;
+
+  @Column({ nullable: true, type: 'text' })
+  description: string;
+
+  // @Column()
+  // category: string;
+
+  @Column('text', { array: true, nullable: true })
+  images: string[];
+
+  @ManyToOne(() => User, (user) => user.skills, {
+    nullable: true,
+    eager: true,
+    onDelete: 'CASCADE',
+  })
+  owner: User;
+}
