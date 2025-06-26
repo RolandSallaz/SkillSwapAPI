f553dd7fab5191e9346e23195d75bf55ba359d3c
Olga Kladova <Olenka-29-04@yandex.ru>
2025-06-26 18:12:41 +0300

    feat(categories): added create

--- src/auth/guards/role-guard.guard.ts
+++ src/auth/guards/role-guard.guard.ts
@@ -11,6 +11,7 @@ export class RoleGuard implements CanActivate {
   canActivate(context: ExecutionContext): boolean {
     const request = context.switchToHttp().getRequest<AuthRequest>();
     const user = request.user;
+    console.log('RoleGuard user:', user);
     if (user.role !== 'admin') {
       throw new ForbiddenException(
         'Доступ запрещен: требуется роль администратора',
--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -1,26 +1,39 @@
 import { Injectable } from '@nestjs/common';
 import { CreateCategoryDto } from './dto/create-category.dto';
-import { UpdateCategoryDto } from './dto/update-category.dto';
+// import { UpdateCategoryDto } from './dto/update-category.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Category } from './entities/category.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class CategoriesService {
-  create(createCategoryDto: CreateCategoryDto) {
-    return 'This action adds a new category';
+  constructor(
+    @InjectRepository(Category)
+    private categoryRepository: Repository<Category>,
+  ) {}
+
+  async create(createCategoryDto: CreateCategoryDto) {
+    const { parent, children } = createCategoryDto;
+    return await this.categoryRepository.save({
+      ...createCategoryDto,
+      parent: parent ? { id: parent } : undefined,
+      children: children.length > 0 ? children.map((id) => ({ id })) : [],
+    });
   }
 
   findAll() {
     return `This action returns all categories`;
   }
 
-  findOne(id: number) {
+  findOne(id: string) {
     return `This action returns a #${id} category`;
   }
 
-  update(id: number, updateCategoryDto: UpdateCategoryDto) {
-    return `This action updates a #${id} category`;
-  }
+  // update(id: string, updateCategoryDto: UpdateCategoryDto) {
+  //   return `This action updates a #${id} category`;
+  // }
 
-  remove(id: number) {
+  remove(id: string) {
     return `This action removes a #${id} category`;
   }
 }
