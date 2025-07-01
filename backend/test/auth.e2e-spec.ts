e738e7441474099f4f9380883d0863e3bbbe0159
Anton Aksenov <Coriolis@inbox.ru>
2025-07-01 21:24:06 +0300

    test(auth): негативные варианты ответов

--- backend/test/auth.e2e-spec.ts
+++ backend/test/auth.e2e-spec.ts
@@ -4,6 +4,8 @@ import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { AppModule } from '../src/app.module';
 import { Server } from 'http';
 import { Response } from 'supertest';
+import { AllExceptionFilter } from '../src/common/all-exception.filter';
+import { ConfigService } from '@nestjs/config';
 
 describe('AuthController (e2e)', () => {
   let app: INestApplication;
@@ -28,6 +30,7 @@ describe('AuthController (e2e)', () => {
     app.useGlobalPipes(
       new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
     );
+    app.useGlobalFilters(new AllExceptionFilter(app.get(ConfigService)));
     await app.init();
     server = app.getHttpServer() as Server;
   });
@@ -48,6 +51,48 @@ describe('AuthController (e2e)', () => {
     expect(res.body).toHaveProperty('user');
     expect(res.body).toHaveProperty('accessToken');
     expect(res.body).toHaveProperty('refreshToken');
+  });
+
+  it('/auth/register (POST) — пустой email (400)', async () => {
+    await request(server)
+      .post('/auth/register')
+      .send({ ...registerDto, email: '' })
+      .expect(400);
+  });
+
+  it('/auth/register (POST) — не валидный email (400)', async () => {
+    await request(server)
+      .post('/auth/register')
+      .send({ ...registerDto, email: 'ddd' })
+      .expect(400);
+  });
+
+  it('/auth/register (POST) — пустой password (400)', async () => {
+    await request(server)
+      .post('/auth/register')
+      .send({ ...registerDto, password: '' })
+      .expect(400);
+  });
+
+  it('/auth/register (POST) — повторная регистрация (409)', async () => {
+    await request(server)
+      .post('/auth/register')
+      .send(registerDto)
+      .expect(409);
+  });
+
+  it('/auth/login (POST) — неверный пароль (401)', async () => {
+    await request(server)
+      .post('/auth/login')
+      .send({ email: registerDto.email, password: 'не верный пароль' })
+      .expect(401);
+  });
+
+  it('/auth/login (POST) — пустой пароль и email (400)', async () => {
+    await request(server)
+      .post('/auth/login')
+      .send({ email: '', password: '' })
+      .expect(400);
   });
 
   it('/auth/login (POST) — успешный логин', async () => {
@@ -71,6 +116,26 @@ describe('AuthController (e2e)', () => {
     refreshToken = body.refreshToken;
   });
 
+  it('/auth/refresh (POST) — нет токена (401)', async () => {
+    await request(server)
+      .post('/auth/refresh')
+      .expect(401);
+  });
+
+  it('/auth/refresh (POST) — не Bearer токен (401)', async () => {
+    await request(server)
+      .post('/auth/refresh')
+      .set('Authorization', refreshToken)
+      .expect(401);
+  });
+
+  it('/auth/refresh (POST) — невалидный refreshToken (401)', async () => {
+    await request(server)
+      .post('/auth/refresh')
+      .set('Authorization', 'Bearer xxx')
+      .expect(401);
+  });
+
   it('/auth/refresh (POST) — успешный refresh', async () => {
     const res: Response = await request(server)
       .post('/auth/refresh')
@@ -80,6 +145,19 @@ describe('AuthController (e2e)', () => {
     expect(res.body).toHaveProperty('user');
     expect(res.body).toHaveProperty('accessToken');
     expect(res.body).toHaveProperty('refreshToken');
+  });
+
+  it('/auth/logout (POST) — без accessToken (401)', async () => {
+    await request(server)
+      .post('/auth/logout')
+      .expect(401);
+  });
+
+  it('/auth/logout (POST) — невалидный accessToken (401)', async () => {
+    await request(server)
+      .post('/auth/logout')
+      .set('Authorization', 'Bearer xxx')
+      .expect(401);
   });
 
   it('/auth/logout (POST) — успешный logout', async () => {
