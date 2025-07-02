0eb8cf8712e8a6eeacfc6f4dd75a1eefff64b4a5
Epanchin Ilya <epnchin.ilya@yandex.ru>
2025-07-02 09:55:50 +0500

    начало переноса notification на отдельный сервер

--- backend/src/config/configuration.ts
+++ backend/src/config/configuration.ts
@@ -41,7 +41,7 @@ export const commonDataSource: DataSourceOptions = {
   name: 'default',
   type: 'postgres',
   host: process.env.DATABASE_HOST || 'localhost',
-  port: parseInt(process.env.INTERIOR_DATABASE_PORT ?? '5432'),
+  port: parseInt(process.env.EXTERNAL_DATABASE_PORT ?? '5432'),
   username: process.env.DATABASE_USER || 'postgres',
   password: process.env.DATABASE_PASSWORD || 'postgres',
   database: process.env.DATABASE_NAME || 'skillswap',
