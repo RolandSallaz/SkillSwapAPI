22d9085e79f238d8bb9f4ce3f1fc07647053c529
Anton Aksenov <Coriolis@inbox.ru>
2025-07-04 22:42:37 +0300

    fix(seeding): при развёртывании нужно заводить только одного юзера - админа

--- backend/src/scripts/users.data.ts
+++ backend/src/scripts/users.data.ts
@@ -4,51 +4,16 @@ import * as dotenv from 'dotenv';
 
 dotenv.config();
 
-const adminUser = {
-  name: process.env.APP_ADMIN_USERNAME || 'admin',
-  email: process.env.APP_ADMIN_EMAIL || 'admin@mail.com',
-  password: process.env.APP_ADMIN_PASSWORD || 'admin',
-  age: 30,
-  city: 'Москва',
-  aboutMe: 'Люблю IT и кофе',
-  gender: Gender.MALE,
-  skills: [],
-  role: UserRole.ADMIN,
-};
-
 export const users: Array<User> = [
-  adminUser,
   {
-    name: 'Александр',
-    email: 'alex@example.com',
-    password: 'hashed_pw_1',
+    name: process.env.APP_ADMIN_USERNAME || 'admin',
+    email: process.env.APP_ADMIN_EMAIL || 'admin@mail.com',
+    password: process.env.APP_ADMIN_PASSWORD || 'admin',
     age: 30,
     city: 'Москва',
     aboutMe: 'Люблю IT и кофе',
     gender: Gender.MALE,
     skills: [],
-    role: UserRole.USER,
-  },
-  {
-    name: 'Мария',
-    email: 'maria@example.com',
-    password: 'hashed_pw_2',
-    age: 25,
-    city: 'Санкт-Петербург',
-    aboutMe: 'Фронтенд разработчик',
-    gender: Gender.FEMALE,
-    skills: [],
-    role: UserRole.USER,
-  },
-  {
-    name: 'Иван',
-    email: 'ivan@example.com',
-    password: 'hashed_pw_3',
-    age: 28,
-    city: 'Новосибирск',
-    aboutMe: 'Бэкенд-энтузиаст',
-    gender: Gender.MALE,
-    skills: [],
-    role: UserRole.USER,
+    role: UserRole.ADMIN,
   },
 ];
